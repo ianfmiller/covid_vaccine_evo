30c7441fe5b047361868a3137e54b496eb75fe77
ifmiller <ifmiller@princeton.edu>
2021-02-09 15:58:26 -0500

    add in 'prop' constant to relate virulence to removal rate

--- SVIC.c
+++ SVIC.c
@@ -1,7 +1,7 @@
 /* file sirmodess.c */
 #include <R.h>
 #include <math.h>
-static double parms[9];
+static double parms[10];
 #define b1 parms[0]
 #define b2 parms[1]
 #define gamma parms[2]
@@ -11,6 +11,7 @@ static double parms[9];
 #define rLn parms[6]
 #define frac_lower parms[7]
 #define v parms[8]
+#define prop parms[9]
 
 
 
@@ -23,7 +24,7 @@ return result;
 /* initializer */
 void initmod(void (* odeparms)(int *, double *))
 {
-  int N=9;
+  int N=10;
   odeparms(&N, parms);
 }
 
@@ -41,15 +42,15 @@ ydot[1] =  -y[1] * (1-rU) * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,r
 
 /* infected--naive */
  
-ydot[2] = y[0] * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v) * y[2];
+ydot[2] = y[0] * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v*prop) * y[2];
 
 /* infected--vaccinated */
   
-ydot[3] = y[1] * (1-rU) * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v*(1-rL)) * y[3];
+ydot[3] = y[1] * (1-rU) * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v*(1-rL)*prop) * y[3];
 
 /* infected--natural immunity */
 
-ydot[4] = y[5] * (1-rUn) * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v*(1-rLn)) * y[4];
+ydot[4] = y[5] * (1-rUn) * (y[2]*betafunc(v,0,0,frac_lower) + y[3]*betafunc(v,rU,rL,frac_lower) + y[4]*betafunc(v,rUn,rLn,frac_lower)) - (gamma + v*(1-rLn)*prop) * y[4];
 
 /* convalescent */
   
