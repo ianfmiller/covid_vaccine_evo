543c6504317424492cf03c9389fd517235cd0326
ifmiller <ifmiller@princeton.edu>
2021-10-22 11:09:34 -0400

    zeroing in on simulation paarams that work consistently

--- figure 3.R
+++ figure 3.R
@@ -46,7 +46,7 @@ res<-4
 ## set global parameters
 
 color.index<-1 #index to match colors and color values to right analysis
-times<-seq(0,365*100,.5)
+times<-seq(0,365*100,1)
          
 gamma<-1/7
 epsilon<-.5
@@ -87,7 +87,7 @@ rLc<-.95 #convalescent class
 
 start.states<-get.states(.25,.1,.001,.5) # set startinng conditions
 
-virulence.steps<-seq(.003,.1,.001)
+virulence.steps<-seq(.003,.2,.001)
 
 if(!file.exists("~/Documents/GitHub/covid_vaccine_evo/sim.data/rUc0.5rLc0.75p.vacc0.1alpha.optim0.00875.RDS"))
 {
@@ -105,22 +105,22 @@ if(!file.exists("~/Documents/GitHub/covid_vaccine_evo/sim.data/rUc0.5rLc0.75p.va
         states<-start.states
         
         parameters0<-c(b1=b1,b2=b2,gamma=gamma,rUv=rUv,rLv=rLv,rUc=rUc,rLc=rLc,rUcv=mean(rUv,1),rLcv=mean(rLv,1),epsilon=epsilon,alpha=0.01,p=p,omega=omega,omegav=omegav,mu=mu,f=f)
-        out0 <- ode(states, times=c(0,0), func = "derivs", parms = parameters0,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "rk4")
+        out0 <- ode(states, times=c(0,0), func = "derivs", parms = parameters0,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "lsoda")
         get.matricies(out0)
         Re.alpha.delta.start<-getR0(Fmat,Vmat)
         
         parameters1<-c(b1=b1,b2=b2,gamma=gamma,rUv=rUv,rLv=rLv,rUc=rUc,rLc=rLc,rUcv=mean(rUv,1),rLcv=mean(rLv,1),epsilon=epsilon,alpha=alpha1,p=p,omega=omega,omegav=omegav,mu=mu,f=f)
-        out1 <- ode(states, times=times, func = "derivs", parms = parameters1,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "rk4")
+        out1 <- ode(states, times=times, func = "derivs", parms = parameters1,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "lsoda",hmax=.01)
         epi.equi.states<-out1[nrow(out1),c("S","V","I_0","I_V","I_C","I_C_V","C","C_V")]
         
-        out2 <- ode(epi.equi.states, times=c(0,0), func = "derivs", parms = parameters0,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "rk4")
+        out2 <- ode(epi.equi.states, times=c(0,0), func = "derivs", parms = parameters0,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method = "lsoda")
         get.matricies(out2)
         Re.alpha.delta.epi.equi<-getR0(Fmat,Vmat)
         
         for (alpha2 in virulence.steps)
         {          
           parameters2<-c(b1=b1,b2=b2,gamma=gamma,rUv=rUv,rLv=rLv,rUc=rUc,rLc=rLc,rUcv=mean(rUv,1),rLcv=mean(rLv,1),epsilon=epsilon,alpha=alpha2,p=p,omega=omega,omegav=omegav,mu=mu,f=f)
-          out3 <- ode(epi.equi.states, times=c(0,0), func = "derivs", parms = parameters2,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31))
+          out3 <- ode(epi.equi.states, times=c(0,0), func = "derivs", parms = parameters2,dllname = "SVIC", initfunc = "initmod",nout=32,outnames=paste0("out",0:31),method="lsoda")
           get.matricies(out3)
           RE.invader<-getR0(Fmat,Vmat)
           RE.invader.vec<-c(RE.invader.vec,RE.invader)
