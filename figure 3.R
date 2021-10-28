02dbd4bd213627eb2e4e16261b880b47254fd8be
ifmiller <ifmiller@princeton.edu>
2021-10-28 15:37:30 -0400

    looking at no waning

--- figure 3.R
+++ figure 3.R
@@ -14,57 +14,57 @@ par(mfrow=c(3,4),oma=c(6,6,0,0))
 ## alpha optim = 0.00875 (intermediate between alpha and delta strain virulence)
 
 ## 50% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.5alpha.optim0.00875.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.5alpha.optim0.00875.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 75% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.75alpha.optim0.00875.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.75alpha.optim0.00875.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 90% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.9alpha.optim0.00875.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.9alpha.optim0.00875.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 99% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.99alpha.optim0.00875.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.99alpha.optim0.00875.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 
 ## alpha optim = 0.01 (delta strain virulence)
 
 ## 50% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.5alpha.optim0.01.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.5alpha.optim0.01.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 75% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.75alpha.optim0.01.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.75alpha.optim0.01.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 90% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.9alpha.optim0.01.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.9alpha.optim0.01.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 99% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.99alpha.optim0.01.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.99alpha.optim0.01.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 
 ## alpha optim = 0.02 (twice as virulent as delta)
 
 ## 50% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.5alpha.optim0.02.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.5alpha.optim0.02.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 75% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.75alpha.optim0.02.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.75alpha.optim0.02.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 90% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.9alpha.optim0.02.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.9alpha.optim0.02.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 ## 99% vaccinated
-plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega10p.vacc0.99alpha.optim0.02.RDS")
+plot.data<-readRDS("~/Documents/GitHub/covid_vaccines_virulence_evolution/sim.data/omega0p.vacc0.99alpha.optim0.02.RDS")
 alpha.ess.mat<-matrix(as.numeric(unlist(plot.data$alpha.ess)),res,res,byrow = T)
 plot.result(alpha.ess.mat,colors1,col.vals1)
 
